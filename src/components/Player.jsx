5b58ec0950c410283540b554b0502c3afbf1fdda
Deniz <denizandac33@gmail.com>
2023-12-14 22:34:29 +0300

    Player name
    +added name algoirthm with useRef

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,18 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [name, setName] = useState("");
+  const ClickHandler = () => {
+    setName(playerName.current.value);
+    playerName.current.value = "";
+  };
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name !== "" ? name : "Unknown Entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={ClickHandler}>Set Name</button>
       </p>
     </section>
   );
